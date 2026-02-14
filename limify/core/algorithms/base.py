9867a773c244e8eebdd6ac3e4e015c45586bc519
pedram-aliniaye-asli <pedram.aliniayeasli@gmail.com>
2026-02-14 01:21:48 -0500

    added asyncalgorithm abstract class

--- limify/core/algorithms/base.py
+++ limify/core/algorithms/base.py
@@ -2,11 +2,20 @@ from abc import ABC, abstractmethod
 from limify.core.context import RequestContext
 from limify.core.plans import Plan
 
+"""
+The allow method:
+Returns True if the request is allowed,
+False if it should be rejected (HTTP 429) or Queued
+"""
+
+
 class Algorithm(ABC):
     @abstractmethod
     def allow(self, key: str, plan: Plan, context: RequestContext) -> bool:
-        """
-        Returns True if the request is allowed,
-        False if it should be rejected (HTTP 429) or Queued
-        """
+        ...
+
+
+class AsyncAlgorithm(ABC):
+    @abstractmethod
+    async def allow(self, key, plan, context) -> bool:
         ...
