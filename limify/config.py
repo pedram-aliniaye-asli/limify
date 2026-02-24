0b32482c6edf97b8eae8e9623e405c745c183045
pedram-aliniaye-asli <pedram.aliniayeasli@gmail.com>
2026-02-24 00:33:42 -0500

    Deleted redis url from config.

--- limify/config.py
+++ limify/config.py
@@ -5,5 +5,4 @@ from typing import List, Dict, Any
 @dataclass
 class LimifyConfig:
     rules: List[Dict[str, Any]]
-    redis_url: str = "redis://localhost:6379"
     default_rule: Dict[str, Any] | None = None
