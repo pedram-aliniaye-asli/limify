7d2c7d28bd9e2044ae512a713b36e513284d2a82
pedram-aliniaye-asli <pedram.aliniayeasli@gmail.com>
2026-03-02 13:09:47 -0500

    Removed markings!

--- limify/__init__.py
+++ limify/__init__.py
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-from limify.core.limify import Limify
-from limify.config import LimifyConfig
-
-__all__ = ["Limify"]
-__all__ = [
-    "Limify",
-    "LimifyConfig",
-]
-=======
 from limify.core.limify import Limify
 from limify.config import LimifyConfig
 
@@ -15,4 +5,3 @@ __all__ = [
     "Limify",
     "LimifyConfig",
 ]
->>>>>>> 18abf6a8da2853dcc328f80e96dab425b3e5f922
